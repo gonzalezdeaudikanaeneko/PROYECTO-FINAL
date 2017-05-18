cfd6837e59655e3b48675b0b9f5950e56d9665cb
Txus <Txus@10.164.62.196>
2017-05-18 16:36:34 +0200

    trabajador

---
+++ Trabajador.cpp
@@ -0,0 +1,8 @@
+/*
+ * Trabajador.cpp
+ *
+ *  Created on: 18 de may. de 2017
+ *      Author: Txus
+ */
+#include "Trabajador.h"
+
---
+++ Trabajador.h
@@ -0,0 +1,19 @@
+/*
+ * Trabajador.h
+ *
+ *  Created on: 18 de may. de 2017
+ *      Author: Txus
+ */
+
+#ifndef TRABAJADOR_H_
+#define TRABAJADOR_H_
+
+class trabajador{
+
+
+private:
+};
+
+
+
+#endif /* TRABAJADOR_H_ */
